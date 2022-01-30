2c5f2bc7fd5c94a86dea0121ccd8ca8d583a5128
Mark Ghebrial <mghebrialjr@icloud.com>
2022-01-30 10:07:31 -0800

    More useful output and faster attempts

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,26 @@ fn main() {
     let params = PasswordParameters::prompt_user();
 
     let num_of_combos = params.num_of_possible_combos();
+
     if prompt_for_boolean(&format!("There are {} possible combinations. Continue?", num_of_combos)) {
-
-        let password: Password = Password::new(&params);
-        println!("{}", password.password);
-
-        for combo in params.combinations() {
-            println!("{}", combo);
-            if combo == password.password {
-                break;
-            }
-        }
-
-        println!("noice");
-
+        brute_force(&params);
     } else {
         println!("OK");
     }  
+}
+
+fn brute_force (params: &PasswordParameters) {
+    let password: Password = Password::new(&params);
+    let num_of_combos = params.num_of_possible_combos();
+
+    let mut attempt_no = 0;
+    for (i, combo) in params.combinations().enumerate() {
+        println!("Trying {}; Elapsed time: _ ; Attempt no. {} of {}", combo, i, num_of_combos);
+        if combo == password.password {
+            attempt_no = i;
+            break;
+        }
+    }
+
+    println!("Cracked password '{}' on attempt {} in _ seconds", password.password, attempt_no);
 }
--- src/password_parameters/combinations.rs
+++ src/password_parameters/combinations.rs
@@ -3,12 +3,14 @@ use super::PasswordParameters;
 
 /// An interator that yields every possible password with
 /// the specified parameters
-pub struct Combinations<'a> {
-    params: &'a PasswordParameters,
-    current_combo: String
+pub struct Combinations {
+    current_combo: String,
+
+    alphabet: HashMap<usize, char>,
+    tebahpla: HashMap<char, usize>,
 }
 
-impl Combinations<'_> {
+impl Combinations {
     pub fn new (params: &PasswordParameters) -> Combinations {
         // Start with the first possible character repeated for
         // the length of the password
@@ -17,36 +19,39 @@ impl Combinations<'_> {
             starting_combo.push(params.get_list_of_possible_characters()[0]);
         }
 
+        // Map the vector of possible characters into two HashMaps
+        // for faster lookup
+        let mut alphabet = HashMap::new();
+        let mut tebahpla = HashMap::new();
+        let list_of_chars = params.get_list_of_possible_characters();
+        for (i, c) in list_of_chars.iter().enumerate() {
+            alphabet.insert(i, *c);
+            tebahpla.insert(*c, i);
+        }
+
         Combinations {
-            params,
-            current_combo: starting_combo
+            current_combo: starting_combo,
+            alphabet,
+            tebahpla,
         }
     }
 
     fn increment (&mut self) {
-        let mut alphabet = HashMap::new();
-        let mut tebahpla = HashMap::new();
-        let list_of_chars = self.params.get_list_of_possible_characters();
-        for (i, c) in list_of_chars.iter().enumerate() {
-            alphabet.insert(i, c);
-            tebahpla.insert(c, i);
-        }
-
         let mut out = String::new();
 
-        let mut rollover = true;
+        let mut rollover = true; // If the current character "rolls over" to the first one, then the one preceeding it must also be incremented
         for c in self.current_combo.chars().rev() {
             if rollover {
-                let mut num = *tebahpla.get(&c).unwrap();
+                let mut num = *self.tebahpla.get(&c).unwrap();
                 num += 1;
 
-                match alphabet.get(&num) {
+                match self.alphabet.get(&num) {
                     Some(c) => {
-                        out.push(**c);
+                        out.push(*c);
                         rollover = false;
                     },
                     None => {
-                        out.push(**alphabet.get(&0).unwrap());
+                        out.push(*self.alphabet.get(&0).unwrap());
                         rollover = true; // Overflow into the next character
                     }
                 }
@@ -60,7 +65,7 @@ impl Combinations<'_> {
     }
 }
 
-impl Iterator for Combinations<'_> {
+impl Iterator for Combinations {
     type Item = String;
 
     fn next(&mut self) -> Option<Self::Item> {
