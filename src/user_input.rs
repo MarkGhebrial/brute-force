a89f1b59d618d544651eb3d513df07f8aaf9c074
Mark Ghebrial <mghebrialjr@icloud.com>
2022-02-13 18:36:01 -0800

    Refactor user input functions

--- src/user_input.rs
+++ src/user_input.rs
@@ -5,44 +5,34 @@ use std::io;
 /// Ask the user for a positive interger, retrying
 /// if they provide invalid input
 pub fn prompt_for_uint (prompt: &str) -> usize {
-    let mut pass_len: Option<usize> = None;
-
     if prompt.len() > 0 {
         println!("{} ", prompt);
     }
 
-    while let None = pass_len {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap(); // Read the user's input
-        let input = input.trim(); // Remove whitespace
-        
-        match input.parse::<usize>() { // Parse the String into an integer
-            Ok(n) => pass_len = Some(n),
-            Err(_) => println!("Please enter a positive integer") // If the string cannot be parsed
-        };
-    }
-
-    pass_len.unwrap()
+    retry_until_ok(
+        || {
+            let input = prompt_for_string(""); // Ask for a string
+            input.parse::<usize>() // Parse that string into a unsigned integer
+        }, 
+        |_| println!("Please enter a positive integer"))
 }
 
 /// Ask the user a yes or no question, returning their
 /// response as a boolean
 pub fn prompt_for_boolean (prompt: &str) -> bool {
-    let mut out: Option<bool> = None;
     println!("{} (yes/no) ", prompt);
-    while let None = out {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap(); // Read the user's input
-        let input = input.trim();
 
-        match input.to_lowercase().as_str() {
-            "yes" | "y" => out = Some(true),
-            "no" | "n" => out = Some(false),
-            _ => println!("Please enter \"yes\" or \"no\"")
-        };
-    }
-
-    out.unwrap()
+    retry_until_ok(
+        || {
+            let input = prompt_for_string("");
+            match input.to_lowercase().as_str() {
+                "yes" | "y" => Ok(true),
+                "no" | "n" => Ok(false),
+                _ => Err(())
+            }
+        },
+        |_| println!("Please enter \"yes\" or \"no\"")
+    )
 }
 
 /// Present the user with a number of options specified 
@@ -68,7 +58,9 @@ pub fn menu_prompt(prompt: &str, options: Vec<&str>) -> usize {
 
 /// Ask the user to input a string
 pub fn prompt_for_string(prompt: &str) -> String {
-    println!("{}", prompt);
+    if prompt.len() > 0 {
+        println!("{}", prompt);
+    }
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap(); // Read the user's input
@@ -79,8 +71,8 @@ pub fn prompt_for_string(prompt: &str) -> String {
 
 /// Keep running the given closure until it returns `Result::Ok<T>`.
 /// 
-/// Every time `action` returns an `Err` (generic `E`), the closure
-/// `handle_err` will be run with the error passed as a prameter.
+/// Every time `action` returns an `Err`, the closure
+/// `handle_err` will be run with that error passed as a prameter.
 pub fn retry_until_ok<F, T, E, H>(action: F, handle_error: H) -> T 
     where 
         F: Fn() -> Result<T, E>, 
